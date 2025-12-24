8ed994dd19d10a988558b853288844d872e4bdc1
DaddyJCub <jacobrzillmer@gmail.com>
2025-12-24 22:46:38 +0000

    Generated by Spark: Fix all reported errors.

--- src/components/NotificationIndicator.tsx
+++ src/components/NotificationIndicator.tsx
@@ -39,5 +39,3 @@ export function NotificationIndicator({ chores, events }: NotificationIndicatorP
     </Badge>
   )
 }
-
-export default NotificationIndicator
--- src/components/OfflineIndicator.tsx
+++ src/components/OfflineIndicator.tsx
@@ -47,5 +47,3 @@ export function OfflineIndicator() {
     </>
   )
 }
-
-export default OfflineIndicator
