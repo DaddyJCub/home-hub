1da70279f28e7835f56d4c8b422cb76c24aa93c7
DaddyJCub <jacobrzillmer@gmail.com>
2025-12-25 15:32:32 +0000

    Edited Spark

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -2,10 +2,10 @@ import { ComponentProps } from "react"
 
 import { cn } from "@/lib/utils"
 
-function Card({ className, ...props }: ComponentProps<"div">) {
-  return (
-    <div
-      data-slot="card"
+      className={cn(
+        cl
+      {.
+  )
       className={cn(
         "bg-card text-card-foreground rounded-xl border shadow-sm",
         className
@@ -16,17 +16,17 @@ function Card({ className, ...props }: ComponentProps<"div">) {
 }
 
 function CardHeader({ className, ...props }: ComponentProps<"div">) {
-  return (
+  )
     <div
       data-slot="card-header"
       className={cn(
         "flex flex-col space-y-1.5 p-6",
         className
       )}
-      {...props}
-    />
   )
-}
+
+  )
+ 
 
 function CardTitle({ className, ...props }: ComponentProps<"div">) {
   return (
@@ -34,19 +34,19 @@ function CardTitle({ className, ...props }: ComponentProps<"div">) {
       data-slot="card-title"
       className={cn("text-lg font-semibold leading-none tracking-tight", className)}
       {...props}
-    />
+      
   )
-}
+ 
 
 function CardDescription({ className, ...props }: ComponentProps<"div">) {
   return (
-    <div
+function
       data-slot="card-description"
       className={cn("text-muted-foreground text-sm", className)}
       {...props}
     />
   )
-}
+
 
 function CardAction({ className, ...props }: ComponentProps<"div">) {
   return (
@@ -58,7 +58,7 @@ function CardAction({ className, ...props }: ComponentProps<"div">) {
       )}
       {...props}
     />
-  )
+  C
 }
 
 function CardContent({ className, ...props }: ComponentProps<"div">) {
@@ -66,27 +66,27 @@ function CardContent({ className, ...props }: ComponentProps<"div">) {
     <div
       data-slot="card-content"
       className={cn("p-6 pt-0", className)}
-      {...props}
+
     />
-  )
+
 }
 
-function CardFooter({ className, ...props }: ComponentProps<"div">) {
-  return (
-    <div
-      data-slot="card-footer"
-      className={cn("flex items-center p-6 pt-0", className)}
-      {...props}
-    />
-  )
-}
 
-export {
-  Card,
-  CardHeader,
-  CardFooter,
-  CardTitle,
-  CardAction,
-  CardDescription,
-  CardContent,
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
